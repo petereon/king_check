103df0b830567caddbbe6d9365a566a9e828253d
Peter Vyboch <peter.vyboch@ibm.com>
2023-04-06 10:03:47 +0200

    feat: move implemented, stubs for pieces

--- src/king_check/_king_check.py
+++ src/king_check/_king_check.py
@@ -1,3 +1,29 @@
+from cytoolz.dicttoolz import assoc_in  # type: ignore
+from cytoolz.dicttoolz import get_in as weird_get_in
+from typing import Any, Dict, Union, cast, Iterable
+
+
+def get_in(d: dict, keys: Iterable[str]) -> dict:
+    return cast(dict, weird_get_in(keys, d))
+
+
+BoardType = dict[str, dict]
+CoordType = tuple[str, str]
+
+
+class Piece:
+    def validate_move(self, from_coord: CoordType, to_coord: CoordType):  # type: ignore
+        pass
+
+
+class Pawn(Piece):
+    def is_starting_position(self, coord):  # type: ignore
+        pass
+
+    def validate_move(self, from_coord: CoordType, to_coord: CoordType):  # type: ignore
+        pass
+
+
 def char_range(c1: str, c2: str) -> list[str]:
     """Generates the characters from `c1` to `c2`, inclusive."""
     return [chr(c) for c in range(ord(c1), ord(c2) + 1)]
@@ -8,15 +34,14 @@ def create_board() -> dict[str, dict]:
     return {i: row for i in char_range('A', 'H')}
 
 
-def set_piece(board: dict[str, dict], col: str, row: str, piece: str, color: str) -> dict[str, dict]:
-    board = board.copy()
-    board[col][row] = {'piece': piece, 'color': color}
-    return board
+def set_piece(board: BoardType, coord: CoordType, piece: str, color: str) -> BoardType:
+    return cast(Dict[str, Dict[Any, Any]], assoc_in(board, coord, {'piece': piece, 'color': color}))
 
 
-def move(*args):  # type: ignore
-    ...
+def move(board: BoardType, coord_from: CoordType, coord_to: CoordType) -> tuple[BoardType, Union[str, None]]:
+    board = assoc_in(board, coord_to, get_in(board, coord_from))
+    return assoc_in(board, coord_from, {}), None
 
 
 def is_check(*args):  # type: ignore
-    ...
+    pass
--- test/test_unit.py
+++ test/test_unit.py
@@ -1,6 +1,6 @@
 from ward import test
 from expycted import expect
-from king_check import create_board, set_piece
+from king_check import create_board, set_piece, move, Pawn
 
 # Entities:
 # - Board
@@ -32,21 +32,44 @@ def _():
 @test('Specified piece is placed on board to defined coordinates')
 def _():
     board = create_board()
-    board = set_piece(board, 'A', '1', 'pawn', 'white')
+    board = set_piece(board, ('A', '1'), 'pawn', 'white')
 
     expect(board['A']['1']).to.be_equal_to({'piece': 'pawn', 'color': 'white'})
 
 
-# coordinates = [
-#     ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8'],
-#     ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8'],
-#     ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8'],
-#     ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8'],
-#     ['E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8'],
-#     ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8'],
-#     ['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8'],
-#     ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8'],
-# ]
+@test("Pawn moves one step ahead from A2 to A1")
+def _():
+    board = create_board()
+    board = set_piece(board, ('A', '2'), 'pawn', 'white')
+    board, err = move(board, ('A', '2'), ('A', '1'))
+
+    expect(board['A']['1']).to.be_equal_to({'piece': 'pawn', 'color': 'white'})
+    expect(board['A']['2']).to.be_equal_to({})
+    expect(err).to.be_falsey()
+
+
+@test("Pawn wants to move diagonally from B2 to A1 but cannot, error is raised")
+def _():
+    board = create_board()
+    board = set_piece(board, ('B', '2'), 'pawn', 'white')
+    board, err = move(board, ('B', '2'), ('A', '1'))
+
+    expect(board['B']['2']).to.be_equal_to({'piece': 'pawn', 'color': 'white'})
+    expect(board['A']['1']).to.be_equal_to({})
+    expect(err).to.be_truthy()
+    expect(err).to.be_equal_to("Pawn cannot move from B2 to A1")
+
+
+@test("Given Pawn is at starting position, it can move 2 steps ahead")
+def _():
+    p = Pawn()
+    expect(p.validate_move(("A", "7"), ("A", "5"))[0]).to.be_true()
+    expect(p.validate_move(("A", "7"), ("A", "5"))[1]).to.be_equal_to(None)
+
+
+@test("Determine if piece is at starting possition")
+def _():
+    pass
 
 
 # @test(f'Board cell with column index 0 and row index 0 is A1')
